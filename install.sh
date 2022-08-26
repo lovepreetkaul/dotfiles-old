bc73cf8eb1fa424d17e289d62ff0f3acd45ef3bb
Lovepreet Singh <pdotl@github.com>
2022-08-26 21:16:57 +0530

    Working neovim install using dotfiles

--- install.sh
+++ install.sh
@@ -1,53 +1,51 @@
 #!/bin/bash
-#!/bin/bash
 
-exec > >(tee -i $HOME/dotfiles_install.log)
-exec 2>&1
 set -x
 
-# Install curl, tar, git, other dependencies if missing
-PACKAGES_NEEDED="\
-    bat \
-    fuse \
-    dialog \
-    apt-utils \
-    libfuse2 \
-    stow"
+RELEASE=${1:-0.7}
 
-install_packages() {
-  if ! dpkg -s ${PACKAGES_NEEDED} > /dev/null 2>&1; then
-      if [ ! -d "/var/lib/apt/lists" ] || [ "$(ls /var/lib/apt/lists/ | wc -l)" = "0" ]; then
-          sudo apt update -y
-      fi
-      sudo echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections
-      sudo apt -y -q install ${PACKAGES_NEEDED}
-  fi
-}
+logfile=$(mktemp)
+exec 3<> "$logfile"
+source_dir=$(mktemp -d)
 
+echo "===> Installing prequisites"
+sudo apt update >&3 2>&1
+sudo apt-get install --no-install-recommends -y stow ripgrep ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen >&3 2>&1
 
-# install latest neovim
-sudo modprobe fuse
-sudo groupadd fuse
-sudo usermod -a -G fuse "$(whoami)"
-wget https://github.com/neovim/neovim/releases/download/v0.5.1/nvim.appimage
-# wget https://github.com/github/copilot.vim/releases/download/neovim-nightlies/appimage.zip
-unzip appimage.zip
-sudo chmod u+x nvim.appimage
-sudo mv nvim.appimage /usr/local/bin/nvim
+echo "===> Cloning neovim"
+pushd "$source_dir" &>/dev/null
+git clone https://github.com/neovim/neovim >&3 2>&1
 
-# Symlinks via Stow
+echo "===> Building neovim $RELEASE and its dependencies"
+cd neovim
+git checkout release-"$RELEASE" >&3 2>&1
+make -j"$(nproc)" >&3 2>&1
 
-# Clean existing
+echo "===> Installing the new neovim"
+sudo make install >&3 2>&1
+
+popd &>/dev/null
+exec 3>&-
+echo "Please see $logfile for additional output"
+
+echo "===> Cleaning existing config."
 rm -f $HOME/.zshrc
 rm -rf $HOME/.config
 
+echo "===> Installing dotfiles"
+dir="$HOME/.config/nvim"
+echo $dir
+return
+if [ ! -d $dir ]; then 
+    mkdir -p $dir
+fi
+stow -vSt ~ vim
+stow -vSt $dir nvim
+stow -vSt ~ zsh
 
-mkdir $HOME/.config
-ln -s "$(pwd)/config/nvim" "$HOME/.config/nvim"
+nvim +'PlugInstall --sync' 
 
-nvim +'PlugInstall --sync' +qa
-
-vim -Es -u $HOME/.vimrc -c "PlugInstall | qa"
+# vim -Es -u $HOME/.vimrc -c "PlugInstall | qa"
 
 sudo chsh -s "$(which zsh)" "$(whoami)"
 
